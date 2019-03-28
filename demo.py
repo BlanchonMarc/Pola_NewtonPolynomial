aa6cd92dca222e5293177d7cc56112b79d171d13
BlanchonMarc <fr.marc.blanchon@gmail.com>
2019-03-28 10:10:38 +0100

    last ver

--- demo.py
+++ demo.py
@@ -8,12 +8,12 @@ see information below
 % The code can only be used for research purpose.
 
 % Please cite the following paper when you use it:
-   % Ning Li, Yongqiang Zhao, Quan Pan, and Seong G. Kong, 
-   % "Demosaicking DoFP images using Newton��s polynomial interpolation and polarization difference model" 
+   % Ning Li, Yongqiang Zhao, Quan Pan, and Seong G. Kong,
+   % "Demosaicking DoFP images using Newton��s polynomial interpolation and polarization difference model"
    % Optics Express 27, 1376-1391 (2019)
 
 %Note:
-%   The code is not optimized and may have bugs. There are ways to improve the efficiency of the algorithms. 
+%   The code is not optimized and may have bugs. There are ways to improve the efficiency of the algorithms.
 %   Your revision and improvement are welcome!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Newton Polynomial Interpolation                          %
@@ -23,27 +23,53 @@ see information below
 %                                                          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 '''
-
+from scipy.misc import bytescale
 import numpy as np
 import cv2
 from newton_polynomial_inteprolation import interpolate as NPI
 
-PATH = 'test.bmp'
+PATH = '/Users/marc/Documents/Dataset/AquisitionImagesFinal/Polarcam/image_00001.png'
 
 if __name__ == '__main__':
-    I = float(cv2.imread(PATH, -1))
+    I = np.double(cv2.imread(PATH, 0))
 
-    (P0, P45, P90, P135) = NPI(I)
+    (PI0, PI45, PI90, PI135) = NPI(I)
 
     ii = 0.5*(PI0 + PI45 + PI90 + PI135)
-    SOP = np.uint8(ii)
+    # print(bytescale(ii))
+    #print(ii[0:10, 0:10])
+
+    SOP = np.double(ii)
+    #print(SOP[0:10, 0:10])
+    out = np.zeros(SOP.shape, np.double)
+    SOP = bytescale(cv2.normalize(SOP, out, 1.0, 0.0,
+                                  cv2.NORM_MINMAX, dtype=cv2.CV_32F))
+    #print(SOP[0:10, 0:10])
+    # print(bytescale(SOP))
+    # print(np.uint8(SOP[0:10, 0:10])PATH = '/Users/marc/Documents/Dataset/AquisitionImagesFinal/Polarcam/image_00001.png')
+    # exit()
     q = PI0 - PI90
     u = PI45 - PI135
-    dolp = sqrt(q**2 + u**2)
+    dolp = np.sqrt(q*q + u*u)
     Dolp = dolp/ii
-    DOLPP = np.uint8(Dolp)
-    aop = (1/2) * np.arctan2(u/q)
-    AOPP = np.uint8(aop)
+    #print(Dolp[1:10, 1:10])
+    DOLPP = np.double(Dolp)
+    #print(DOLPP[1:10, 1:10])
+    out = np.zeros(DOLPP.shape, np.double)
+    DOLPP = bytescale(cv2.normalize(DOLPP, out, 1.0, 0.0,
+                                    cv2.NORM_MINMAX, dtype=cv2.CV_32F))
+    #print(DOLPP[1:10, 1:10])
+    aop = (1/2) * np.arctan2(u, q)
+
+    AOPP = np.double(aop)
+
+    out = np.zeros(AOPP.shape, np.double)
+    AOPP = bytescale(cv2.normalize(AOPP, out, 1.0, 0.0,
+                                   cv2.NORM_MINMAX, dtype=cv2.CV_32F))
+
+    #SOP = np.uint8(SOP)
+    #DOLPP = np.uint8(DOLPP)
+    #AOPP = np.uint8(AOPP)
 
     cv2.imshow('Intensity', SOP)
     cv2.waitKey(0)
--- newton_polynomial_inteprolation.py
+++ newton_polynomial_inteprolation.py
@@ -31,26 +31,31 @@ import numpy as np
 
 
 def interpolate(I):
+    I = np.double(I)
     (m, n) = I.shape
 
     R = np.zeros((m, n, 4))
 
     # Labeling different polatiration channels
-    O = np.zeros((m, n))
+    O = np.zeros((m, n), dtype=int)
 
     step = 2
-    O[0:m:step, 0:n:step] = 0
-    O[0:m:step, 1:n:step] = 1
-    O[1:m:step, 1:n:step] = 2
-    O[1:m:step, 0:n:step] = 3
+    O[0:m+1:step, 0:n+1:step] = 0
+    O[0:m+1:step, 1:n+1:step] = 1
+    O[1:m+1:step, 1:n+1:step] = 2
+    O[1:m+1:step, 0:n+1:step] = 3
 
     # Store intermediate results
-
     Y1 = I
     Y2 = I
+    #Y1 = np.double(I)
+    #Y2 = np.double(I)
 
-    for index in R.shape[2]:
-        R[:, :, index] = I
+    # for index in range(R.shape[2]):
+    R[:, :, 0] = I
+    R[:, :, 1] = I
+    R[:, :, 2] = I
+    R[:, :, 3] = I
 
     '''
     % Stage one interpolation: interpolate vertically for case Fig.6(b),
@@ -59,10 +64,10 @@ def interpolate(I):
     % code.
     '''
 
-    for i in range(3, m-2):
-        for j in range(3, n-2):
+    for i in range(3, m-4):
+        for j in range(3, n-4):
             R[i, j, O[i, j]] = I[i, j]
-            R[i, y, O[i, j+1]] = 0.5*I[i, j] + 0.0625*I[i, j-3] - 0.25*I[i, j-2] + \
+            R[i, j, O[i, j+1]] = 0.5*I[i, j] + 0.0625*I[i, j-3] - 0.25*I[i, j-2] + \
                 0.4375*I[i, j-1] + 0.4375*I[i, j+1] - \
                 0.25*I[i, j+2] + 0.0625*I[i, j+3]
             R[i, j, O[i+1, j]] = 0.5*I[i, j] + 0.0625*I[i-3, j] - 0.25*I[i-2, j] + \
@@ -74,18 +79,17 @@ def interpolate(I):
             Y2[i, j] = 0.5*I[i, j] + 0.0625*I[i-3, j+3] - 0.25*I[i-2, j+2] + 0.4375 * \
                 I[i-1, j+1] + 0.4375*I[i+1, j-1] - \
                 0.25*I[i+2, j-2] + 0.0625*I[i+3, j-3]
-
     # One can adjust for better result
     thao = 5.8
     # Fusion of the estimations with edge classifier for case Fig.6(a).
 
-    for i in range(3, m-2):
-        for j in range(3, m-2):
-            pha1 = 0
-            pha2 = 0
+    for i in range(3, m-4):
+        for j in range(3, m-4):
+            pha1 = 0.0
+            pha2 = 0.0
 
-            for k in range(-2, 2, 2):
-                for l in range(-2, 2, 2):
+            for k in range(-2, 3, 2):
+                for l in range(-2, 3, 2):
                     pha1 = pha1 + abs(Y1[i+k, j+l] - I[i+k, j+l])
                     pha2 = pha2 + abs(Y2[i+k, j+l] - I[i+k, j+l])
 
@@ -98,7 +102,7 @@ def interpolate(I):
                     abs(2*I[i, j] - I[i-2, j-2] - I[i+2, j+2])
                 d2 = abs(I[i+1, j-1] - I[i-1, j+1]) + \
                     abs(2*I[i, j] - I[i+2, j-2] - I[i-2, j+2])
-                epsl = np.finfo(float).eps
+                epsl = 0.000000000000001
                 w1 = 1/(d1 + epsl)
                 w2 = 1/(d2+epsl)
                 R[i, j, O[i+1, j+1]] = (w1*Y1[i, j] + w2*Y2[i, j])/(w1 + w2)
@@ -113,26 +117,32 @@ def interpolate(I):
     # Stage two interpolation: interpolate horizontally for case Fig.6(b),
     # interpolate vertically for case Fig.6(c).
 
-    for i in range(3, m-2):
-        for j in range(3, n-2):
+    for i in range(3, m-4):
+        for j in range(3, n-4):
             XX1[i, j] = R[i, j, O[i, j+1]]
-            XX2[i, j] = 0.5*I[i, j] + 0.0625*R[i-3, j, O[i, j+1]] - 0.25*I[i-2, j] + 0.4375*R[i-1, j,
-                                                                                              O[i, j+1]] + 0.4375*R[i+1, j, O[i, j+1]] - 0.25*I[i+2, j] + 0.0625*R[i+3, j, O[i, j+1]]
+            XX2[i, j] = 0.5*I[i, j] + 0.0625 * \
+                R[i-3, j, O[i, j+1]] - 0.25*I[i-2, j]
+            XX2[i, j] = XX2[i, j] + 0.4375 * \
+                R[i-1, j, O[i, j+1]] + 0.4375*R[i+1, j, O[i, j+1]]
+            XX2[i, j] = XX2[i, j] - 0.25*I[i+2, j] + 0.0625*R[i+3, j, O[i, j+1]]
             YY1[i, j] = R[i, j, O[i+1, j]]
-            YY2[i, j] = 0.5*I[i, j] + 0.0625*R[i, j-3, O[i+1, j]] - 0.25*I[i, j-2] + 0.4375*R[i, j-1,
-                                                                                              O[i+1, j]] + 0.4375*R[i, j+1, O[i+1, j]] - 0.25*I[i, j+2] + 0.0625*R[i, j+3, O[i+1, j]]
+            YY2[i, j] = 0.5*I[i, j] + 0.0625 * \
+                R[i, j-3, O[i+1, j]] - 0.25*I[i, j-2]
+            YY2[i, j] = YY2[i, j] + 0.4375 * \
+                R[i, j-1, O[i+1, j]] + 0.4375*R[i, j+1, O[i+1, j]]
+            YY2[i, j] = YY2[i, j] - 0.25*I[i, j+2] + 0.0625*R[i, j+3, O[i+1, j]]
 
     # Fusion of the estimations with edge classifier for case Fig.6(b) and Fig.6(c).
 
-    for i in range(3, m-2):
-        for j in range(3, n-2):
-            pha1 = 0
-            pha2 = 0
+    for i in range(3, m-4):
+        for j in range(3, n-4):
+            pha1 = 0.0
+            pha2 = 0.0
 
-            for k in range(-2, 2, 2):
-                for l in range(-2, 2, 2):
-                    pha1 = pha1 + abs(XX1[i+k, j+l] - I[i+k, j+l]]
-                    pha2 = pha2 + abs(XX2[i+k, j+l] - I[i+k, j+l]]
+            for k in range(-2, 3, 2):
+                for l in range(-2, 3, 2):
+                    pha1 = pha1 + abs(XX1[i+k, j+l] - I[i+k, j+l])
+                    pha2 = pha2 + abs(XX2[i+k, j+l] - I[i+k, j+l])
 
             if (pha1 / pha2) > thao:
                 R[i, j, O[i, j+1]] = XX2[i, j]
@@ -143,18 +153,18 @@ def interpolate(I):
                     abs(2*I[i, j] - I[i, j-2] - I[i, j+2])
                 d2 = abs(I[i+1, j] - I[i-1, j]) + \
                     abs(2*I[i, j] - I[i+2, j] - I[i-2, j])
-                epsl = np.finfo(float).eps
+                epsl = 0.000000000000001
                 w1 = 1/(d1 + epsl)
                 w2 = 1/(d2 + epsl)
                 R[i, j, O[i, j+1]] = (w1*XX1[i, j] + w2*XX2[i, j])/(w1 + w2)
 
-            pha1 = 0
-            pha2 = 0
+            pha1 = 0.0
+            pha2 = 0.0
 
-            for k in range(-2, 2, 2):
-                for l in range(-2, 2, 2):
-                    pha1 = pha1 + abs(YY1[i+k, j+l] - I[i+k, j+l]]
-                    pha2 = pha2 + abs(YY2[i+k, j+l] - I[i+k, j+l]]
+            for k in range(-2, 3, 2):
+                for l in range(-2, 3, 2):
+                    pha1 = pha1 + abs(YY1[i+k, j+l] - I[i+k, j+l])
+                    pha2 = pha2 + abs(YY2[i+k, j+l] - I[i+k, j+l])
 
             if (pha1 / pha2) > thao:
                 R[i, j, O[i+1, j]] = YY2[i, j]
@@ -165,7 +175,7 @@ def interpolate(I):
                     abs(2*I[i, j] - I[i, j-2] - I[i, j+2])
                 d2 = abs(I[i+1, j] - I[i-1, j]) + \
                     abs(2*I[i, j] - I[i+2, j] - I[i-2, j])
-                epsl = np.finfo(float).eps
+                epsl = 0.000000000000001
                 w1 = 1/(d1 + epsl)
                 w2 = 1/(d2 + epsl)
                 R[i, j, O[i, j+1]] = (w1*YY1[i, j] + w2*YY2[i, j])/(w1 + w2)
